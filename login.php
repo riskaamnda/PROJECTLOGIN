552eae1c992505d66bd2b0e280f0f9adc70b53e9
riska amanda <riskaamanda6778@gmail.com>
2025-09-26 11:24:35 +0700

    Proses Login

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+// Proses login saat form dikirim
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    // Login sederhana (username: admin, password: 123)
+    if ($username === 'admin' && $password === '123') {
+        $_SESSION['username'] = $username;
+        $_SESSION['role'] = 'Dosen';
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<h2>Form Login</h2>
+   <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
+<form method="post">
+    Username: <input type="text" name="username" required><br><br> 
+    Password: <input type="password" name="password" required><br><br> 
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>          
+
+
